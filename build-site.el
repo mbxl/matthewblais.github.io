b6b71cff10b12076f8c7d4bf11f395b113aad533
Matthew Blais <mpblaze@protonmail.com>
2021-10-13 17:46:24 -0700

    Moved everything to a single org file to tangle.

--- build-site.el
+++ build-site.el
@@ -1,32 +1,8 @@
 ;; Require org publishing system
 (require 'ox-publish)
-(require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-;; Define the publishing project
-(setq org-publish-project-alist
-      (list
-       (list "mbxl.github.io"
-             :recursive t
-             :base-directory "./content"
-             :publishing-directory "./docs"
-	     :publishing-function 'org-html-publish-to-html
-	     :with-author nil
-	     :with-creator nil
-	     :with-toc nil
-	     :section-numbers nil
-	     :time-stamp-file nil)))
 
-(setq org-html-validation-link nil
-      org-html-head-include-scripts nil
-      org-html-head-include-default-style nil
-      org-html-head "<link rel=\"stylesheet\" href=\"style.css\" />")
-
-(org-babel-tangle-file "style-script.org")
-
-;; Generate the site output
-(org-publish-all t)
+;; Tangle code blocks in org file
+(org-babel-tangle-file "site.org")
 
 ;; Success message
 (message "Build complete!")
